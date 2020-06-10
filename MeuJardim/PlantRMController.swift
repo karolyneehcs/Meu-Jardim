2007df51306a8f66a0a103dad9864035dd786e8e
Cristiano Coutinho <criscoutinho44@gmail.com>
2020-06-10 11:49:15 -0300

    Add Label and TextFiel

---
+++ MeuJardim/PlantRMController.swift
@@ -0,0 +1,19 @@
+//
+//  PlantRMController.swift
+//  MeuJardim
+//
+//  Created by Cristiano Coutinho on 10/06/20.
+//  Copyright © 2020 Jhennyfer Rodrigues de Oliveira. All rights reserved.
+//
+
+import UIKit
+
+class PlantRMController: UIViewController{
+    
+    @IBOutlet weak var popularPlantField: UITextField!
+    
+    @IBOutlet weak var scientificPlantField: UITextField!
+    
+
+    
+}
