757f2352fbc08559c344a5f3aa7b3278c6edffd8
Cristiano Coutinho <criscoutinho44@gmail.com>
2020-06-12 11:26:45 -0300

    Add ImageView

--- MeuJardim/PlantRMController.swift
+++ MeuJardim/PlantRMController.swift
@@ -10,10 +10,34 @@ import UIKit
 
 class PlantRMController: UIViewController{
     
+    @IBOutlet weak var imagePickerButton: UIButton!
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
     @IBOutlet weak var popularPlantField: UITextField!
     
     @IBOutlet weak var scientificPlantField: UITextField!
     
-
+    var imagePicker:ImagePicker!
     
+    override func viewDidLoad() {
+          super.viewDidLoad()
+          self.imagePicker = ImagePicker(presentationController: self, delegate: self)
+          
+      }
+    
+    @IBAction func showImagePicker(_ sender: UIButton) {
+        
+        self.imagePicker.present(from: sender)
+    }
+    
+}
+
+extension PlantRMController: ImagePickerDelegate {
+     func didSelect(image: UIImage?) {
+           guard let image = image else {
+               return
+           }
+           self.imageView.image = image
+       }
 }
