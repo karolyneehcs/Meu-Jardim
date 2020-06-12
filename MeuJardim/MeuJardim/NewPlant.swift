4b299b926899c2a4ca24f2f5b94c6274afc71887
jhennyferOliveira <jhennyfer519@gmail.com>
2020-06-12 10:27:58 -0300

    criando tela de adicionar nova planta

--- MeuJardim/MeuJardim/NewPlant.swift
+++ MeuJardim/MeuJardim/NewPlant.swift
@@ -9,22 +9,27 @@
 import UIKit
 
 class NewPlant: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        navigationController?.navigationBar.prefersLargeTitles = false
+        
         // Do any additional setup after loading the view.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func willMove(toParent parent: UIViewController?) {
+        navigationItem.largeTitleDisplayMode = .always
+        navigationController?.navigationBar.prefersLargeTitles = true
     }
-    */
-
+    
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
