274dc77d77f4944407451b098b5b8b3123d9e090
Ana Karolina <anakarolina1966@icloud.com>
2020-06-24 10:07:50 -0300

    Revert "Dev"

--- MeuJardim/MeuJardim/AppDelegate.swift
+++ MeuJardim/MeuJardim/AppDelegate.swift
@@ -15,7 +15,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        configureForTests()
         return true
     }
 
@@ -31,13 +30,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called when the user discards a scene session.
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
-    }
-    
-    private func configureForTests (){
-        if CommandLine.arguments.contains("-reset"){
-            let defaultsName = Bundle.main.bundleIdentifier!
-            UserDefaults.standard.removePersistentDomain(forName: defaultsName)
-        }
     }
 
 
