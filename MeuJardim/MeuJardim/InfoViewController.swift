5c1d0cc9add16ce17679ef520f1f4ecdc7745ba0
jhennyferOliveira <jhennyfer519@gmail.com>
2020-06-27 10:43:01 -0300

    alterando tela de info

---
+++ MeuJardim/MeuJardim/InfoViewController.swift
@@ -0,0 +1,30 @@
+//
+//  InfoViewController.swift
+//  MeuJardim
+//
+//  Created by Jhennyfer Rodrigues de Oliveira on 27/06/20.
+//  Copyright © 2020 Jhennyfer Rodrigues de Oliveira. All rights reserved.
+//
+
+import UIKit
+
+class InfoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
